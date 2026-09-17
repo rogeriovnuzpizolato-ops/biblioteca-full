8a4e6b0835d9091f917567b9dfb63c9b61fdb7f7
Rogerio Luiz <rogeriovnuzpizolato@gmail.com>
2026-09-16 22:36:54 -0300

    feat: adicao do listener na lista-livros que descobre qual botao foi clicado e qual acao deve ser tomada

--- script/main.js
+++ script/main.js
@@ -4,4 +4,30 @@ async function init() {
     renderizarLivros(livros);
 }
 
+const listaLivros = document.getElementById("lista-livros");
+
+listaLivros.addEventListener("click", async (evento) => {
+    // descobre qual botao foi clicado e qual ação deve ser executada 
+    const botao = evento.target.closest("button[data-action]");
+
+    if (!botao) return;
+
+    const acao = botao.dataset.action;
+
+    // acha a div pai onde esta o id do livro e pegao id
+    const acoesDiv = botao.closest("[data-id]");
+    const id = acoesDiv.dataset.id;
+
+    // decide qual função chamar, baseado no valor de data-action
+    if (acao === "alugar") {
+        await alugarLivro(id);
+    } else if (acao === "devolver") {
+        await devolverLivro(id);
+    } else if (acao === "deletar") {
+        await deletarLivro(id);
+    }
+
+    init();
+});
+
 init();
